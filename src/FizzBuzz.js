7d08f958bd14fb1717589f4c3d5b688848ece2cd
Cody13X <eyannick13@gmail.com>
2015-11-24 20:21:38 +0100

    Finalisation de FissBuzz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,12 @@
 function fizzBuzz(i) {
-  
+  var Fizz = 'Fizz';
+  var Buzz = 'Buzz';
+  var FizzBuzz = 'FizzBuzz';
+  if(i%3 === 0) return Fizz;
+  if(i%5 === 0) return Buzz;
+  if(i%15 === 0) return FizzBuzz;
+
+  return i;
 }
 
 module.exports = fizzBuzz;
