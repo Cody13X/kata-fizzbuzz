5791aacffc0ded03d8609403975cfb3029d4b6d1
Cody13X <eyannick13@gmail.com>
2015-11-24 21:47:51 +0100

    Corrections

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -8,12 +8,12 @@ describe("FizzBuzz", function() {
     expect(fizzBuzz(5)).toEqual('Buzz');
     expect(fizzBuzz(40)).toEqual('Buzz');
   });
-/*  it("returns 'FizzBuzz' for multiples of 15", function() {
+  it("returns 'FizzBuzz' for multiples of 15", function() {
     expect(fizzBuzz(15)).toEqual('FizzBuzz');
     expect(fizzBuzz(150)).toEqual('FizzBuzz');
   });
   it("returns the argument if not divisible by 3 or 5", function() {
     expect(fizzBuzz(1)).toEqual(1);
     expect(fizzBuzz(101)).toEqual(101);
-  });*/
+  });
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,10 +1,7 @@
 function fizzBuzz(i) {
-  /*var Fizz = 'Fizz';
-  var Buzz = 'Buzz';
-  var FizzBuzz = 'FizzBuzz';*/
+  if(i%15 === 0) return 'FizzBuzz';
   if(i%3 === 0) return 'Fizz';
   if(i%5 === 0) return 'Buzz';
-  if(i%15 === 0) return 'FizzBuzz';
 
   return i;
 }
